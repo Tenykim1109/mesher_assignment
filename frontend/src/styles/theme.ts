a32771b46a528a547e65c3f8736ac88a5bb93b6b
Minjeong Kim <lona573@gmail.com>
2022-08-31 15:12:14 +0900

    Initial commit

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,10 @@
+const color = {
+  white: 'white',
+  black: 'black',
+};
+
+export const theme = {
+  color,
+};
+
+export type Theme = typeof theme;
---
+++ frontend/src/typing/styled.d.ts
@@ -0,0 +1,12 @@
+import { Theme } from '../styles/theme';
+import { CSSProp } from 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends Theme {}
+}
+
+declare module 'react' {
+  interface Attributes {
+    css?: CSSProp | CSSObject;
+  }
+}
